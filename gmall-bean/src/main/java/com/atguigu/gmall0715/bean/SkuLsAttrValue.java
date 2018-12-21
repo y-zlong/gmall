fc635e5b8957527cac693c1bf8b3de5606a0c91f
yzlong <1185699134>
2018-12-21 16:25:26 +0800

    剩余支付模块

---
+++ gmall-bean/src/main/java/com/atguigu/gmall0715/bean/SkuLsAttrValue.java
@@ -0,0 +1,17 @@
+package com.atguigu.gmall0715.bean;
+
+import java.io.Serializable;
+/*
+平台属性值ID
+ */
+public class SkuLsAttrValue implements Serializable {
+    String valueId;
+
+    public String getValueId() {
+        return valueId;
+    }
+
+    public void setValueId(String valueId) {
+        this.valueId = valueId;
+    }
+}
